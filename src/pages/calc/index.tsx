0fe86d5f3829c147e7c1dbac03b53a64ecbf52aa
Hyunsu Lim <hyunsu.lim01@gmail.com>
2021-06-13 17:06:41 +0900

    Add score display

--- src/pages/calc/index.tsx
+++ src/pages/calc/index.tsx
@@ -11,7 +11,7 @@ import {
   HStack,
   VStack,
 } from '@chakra-ui/react';
-import { parseInput } from './utils';
+import { parseInput, calcScore } from './utils';
 import { Tile } from './types';
 
 const TileImg: React.FC<{ tile: Tile | null, }> = ({ tile }) => {
@@ -52,35 +52,48 @@ const Calc: React.FC = () => {
       />
       <Button onClick={onClickCalc}>계산하기</Button>
       {result !== undefined && (
-        result.ok ? (
-          <VStack mt={8}>
-            <Heading size="md">결과</Heading>
-            <HStack>
-              <VStack>
-                <Text>손패</Text>
-                <HStack spacing={0}>
-                  {result.hand.map((t) => <TileImg key={Math.random()} tile={t} />)}
-                </HStack>
-              </VStack>
-              {result.nakiTiles.map((n, i) => (
-                <VStack key={Math.random()}>
-                  <Text>{`후로 ${i + 1}`}</Text>
+        result.ok ? (() => {
+          const score = calcScore(result.hand, result.nakiTiles, result.agariTile, result.agariType);
+          return (
+            <VStack mt={8}>
+              <Heading size="md">결과</Heading>
+              <HStack>
+                <VStack>
+                  <Text>손패</Text>
                   <HStack spacing={0}>
-                    {n.tiles.map((t, k) => (
-                      <TileImg key={Math.random()} tile={!n.open && (k === 0 || k === 3) ? null : t} />
-                    ))}
+                    {result.hand.map((t) => <TileImg key={Math.random()} tile={t} />)}
                   </HStack>
                 </VStack>
-              ))}
-            </HStack>
-            <Text>오름패 {result.agariType === 'tsumo' ? '쯔모' : '론'}</Text>
-            <TileImg tile={result.agariTile} />
-          </VStack>
-        ) : (
+                {result.nakiTiles.map((n, i) => (
+                  <VStack key={Math.random()}>
+                    <Text>{`후로 ${i + 1}`}</Text>
+                    <HStack spacing={0}>
+                      {n.tiles.map((t, k) => (
+                        <TileImg key={Math.random()} tile={!n.open && (k === 0 || k === 3) ? null : t} />
+                      ))}
+                    </HStack>
+                  </VStack>
+                ))}
+              </HStack>
+              <Text>오름패 {result.agariType === 'tsumo' ? '쯔모' : '론'}</Text>
+              <TileImg tile={result.agariTile} />
+              {score.type === 'error' && (
+                <Text>{score.msg}</Text>
+              )}
+              {score.type === 'yakuman' && (
+                <>
+                  <Heading size="md">역</Heading>
+                  {score.list.map((y) => <Text>{y}</Text>)}
+                  <Heading size="lg">{score.multiplier === 1 ? '역만' : `${score.multiplier}배 역만`}</Heading>
+                </>
+              )}
+            </VStack>
+          );
+        })() : (
           <Text>{result.msg}</Text>
         )
       )}
-      <Text>
+      <Text pt={12}>
         패 이미지는&nbsp;
         <Link href="http://www.martinpersson.org/" color="green.400" isExternal>여기</Link>
         에서 가져옴
